d52c2f20a477503c383d0055fe718276bd702e93
Alessandro Petraro <alessandro.petraro@gmail.com>
2017-12-09 15:23:24 +0100

    Changed config.js.

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 // Hold application secrets and config
 module.exports = {
     api_server: {
-        protocol: process.env.MYBOOKER_API_PROTOCOL || 'https',
-        api_version: process.env.MYBOOKER_API_VERSION || 'v1',
-        host: process.env.MYBOOKER_API_HOST || 'api.your-host.net',
-        port: parseInt(process.env.MYBOOKER_API_PORT, 10) || 3090
+        protocol: process.env.API_PROTOCOL || 'https',
+        api_version: process.env.API_VERSION || 'v1',
+        host: process.env.API_HOST || 'api.your-host.net',
+        port: parseInt(process.env.API_PORT, 10) || 3090
     },
-
     secret: 'your-secret-here'
 };
